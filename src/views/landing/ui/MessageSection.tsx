ecffa026f148560b0c24d016f5917b57436d5228
sunhwaaRj <pickle02@sookmyung.ac.kr>
2026-07-15 20:24:09 +0900

    #7 [FEAT] 랜딩페이지 메세지 섹션

--- src/views/landing/ui/MessageSection.tsx
+++ src/views/landing/ui/MessageSection.tsx
@@ -2,9 +2,17 @@ import { LandingContainer } from "@/views/landing/ui/LandingContainer";
 
 export function MessageSection() {
   return (
-    <LandingContainer className="bg-white">
-      <div className="flex flex-col items-center justify-center gap-3 py-16">
-        중간 멘트 콘텐츠입니다.
+    <LandingContainer className="gradient-bg-04">
+      <div className="flex flex-col items-center justify-center gap-2 py-12">
+        <div className="flex gap-2">
+          <span className="px-2 py-1 rounded-full bg-gray-0 body-11">
+            프로젝트 회고
+          </span>
+          <span className="px-2 py-1 rounded-full bg-gray-0 body-11">
+            셀프 브랜딩
+          </span>
+        </div>
+        <div className="body-7 whitespace-break-spaces text-center">{`나만의 소프트스킬로\n다양한 활동을 시작해보세요!`}</div>
       </div>
     </LandingContainer>
   );
