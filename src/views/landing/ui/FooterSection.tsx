b746e86233899a06f1314caa45b40908d2e5e244
sunhwaaRj <pickle02@sookmyung.ac.kr>
2026-07-23 20:15:45 +0900

    #7 [FEAT] 랜딩페이지 푸터 링크 연결

--- src/views/landing/ui/FooterSection.tsx
+++ src/views/landing/ui/FooterSection.tsx
@@ -6,13 +6,27 @@ export function FooterSection() {
     <LandingContainer className="bg-gray-900">
       <section className="flex items-center justify-center py-4">
         <div className="w-60 flex flex-col items-start gap-2">
-          <Image
-            src="/images/teamficial.svg"
-            alt="teamficial"
-            width={250}
-            height={72}
-            className="h-6 w-auto"
-          />
+          <div className="flex w-full justify-between items-center">
+            <Image
+              src="/images/logo-white.svg"
+              alt="푸터 팀피셜 로고"
+              width={100}
+              height={13}
+              loading="lazy"
+            />
+            <a
+              href="https://www.instagram.com/teamficial.official/"
+              className="cursor-pointer"
+            >
+              <Image
+                src="/images/instagram.svg"
+                alt="인스타그램 연결 링크"
+                width={13}
+                height={13}
+                loading="lazy"
+              />
+            </a>
+          </div>
           <div className="flex flex-col gap-0.5 text-gray-700 body-11">
             <p>©TEAMFICIAL ALL RIGHT RESERVED</p>
             <div className="flex gap-2">
@@ -26,11 +40,16 @@ export function FooterSection() {
             </div>
           </div>
           <div className="flex gap-2 body-11 text-gray-0">
-            {/* TODO: 실제 약관 URL 확정되면 연결 */}
-            <a href="#" className="cursor-pointer">
+            <a
+              href="https://season-daughter-d26.notion.site/33da1c1c2a9980f4999fd34458061625?source=copy_link"
+              className="cursor-pointer"
+            >
               개인정보처리방침
             </a>
-            <a href="#" className="cursor-pointer">
+            <a
+              href="https://season-daughter-d26.notion.site/3a6a1c1c2a9980b88bfec694ba28f7cd?source=copy_link"
+              className="cursor-pointer"
+            >
               서비스이용약관
             </a>
           </div>
