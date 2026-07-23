a3b588158ddd1bc0276d56cf0c89edba02473bc6
sunhwaaRj <pickle02@sookmyung.ac.kr>
2026-07-23 20:54:22 +0900

    #7 [FEAT] 팀피셜록 경로 연결

--- src/views/landing/ui/HeroSection.tsx
+++ src/views/landing/ui/HeroSection.tsx
@@ -1,6 +1,7 @@
 import { LandingContainer } from "@/views/landing/ui/LandingContainer";
 import { Header } from "./Header";
 import Image from "next/image";
+import Link from "next/link";
 
 export function HeroSection() {
   return (
@@ -18,9 +19,12 @@ export function HeroSection() {
         <div className="flex flex-col items-center text-center">
           <p className="body-5">쌓여가는 나의 소프트스킬</p>
           <p className="body-9 pt-1 pb-4 whitespace-break-spaces">{`간단한 링크 공유로 나와 함께한 팀원들로부터\n소프트스킬 키워드를 얻을 수 있어요`}</p>
-          <button className="body-9 text-gray-0 w-30 py-2 bg-gray-700 rounded-lg cursor-pointer">
+          <Link
+            href="/teamficiallog"
+            className="body-9 text-gray-0 w-30 py-2 bg-gray-700 rounded-lg cursor-pointer"
+          >
             팀피셜 시작하기
-          </button>
+          </Link>
         </div>
         <div className="flex flex-wrap justify-center gap-2.5">
           <Image
